c052c4f0c75350a445c0b08a32809c3ea8b3e39c
siisgoo <siisgoo@mail.ru>
2022-03-22 01:26:41 +0300

    deleted:    lib/Debug/Crypto 	deleted:    lib/Debug/Crypto.0.9.0 	deleted:    lib/Debug/Database 	deleted:    lib/Debug/Database.0.9.0 	deleted:    lib/Debug/Logger 	deleted:    lib/Debug/Logger.0.1.2 	deleted:    lib/Debug/Math 	deleted:    lib/Debug/Math.1.0.0 	deleted:    lib/Debug/Network 	deleted:    lib/Debug/Network.0.2.0 	deleted:    lib/Debug/PagesManager 	deleted:    lib/Debug/PagesManager.0.4.0 	deleted:    lib/Debug/Widgets 	deleted:    lib/Debug/Widgets.0.1.9 	deleted:    lib/Debug/iiServer 	deleted:    lib/Debug/iiServer.0.0.9 	deleted:    lib/include/ClickableLabel.hpp 	deleted:    lib/include/CmdError.hpp 	deleted:    lib/include/CreateTables.hpp 	deleted:    lib/include/Database.hpp 	deleted:    lib/include/Hash.hpp 	deleted:    lib/include/Matrix.hpp 	deleted:    lib/include/Matrix.inl 	deleted:    lib/include/NotifyItem.hpp 	deleted:    lib/include/NotifyItemFactory.hpp 	deleted:    lib/include/NotifyManager.hpp 	deleted:    lib/include/NotifyMsgItem.hpp 	deleted:    lib/include/NotifyMsgItemFactory.hpp 	deleted:    lib/include/NotifyProgressItem.hpp 	deleted:    lib/include/NotifyProgressItemFactory.hpp 	deleted:    lib/include/ObjectType.hpp 	deleted:    lib/include/PagesManager.hpp 	deleted:    lib/include/QLedIndicator.hpp 	deleted:    lib/include/Query.hpp 	deleted:    lib/include/Role.hpp 	deleted:    lib/include/SslClientBase.hpp 	deleted:    lib/include/SslServer.hpp 	deleted:    lib/include/Types.hpp 	deleted:    lib/include/Utility.hpp 	deleted:    lib/include/Vectors.hpp 	deleted:    lib/include/Vectors.inl 	deleted:    lib/include/command.hpp 	deleted:    lib/include/iiClient.hpp 	deleted:    lib/include/iiNPack.hpp 	deleted:    lib/include/iiServer.hpp 	deleted:    lib/include/logger.hpp 	deleted:    lib/include/tables.hpp

--- lib/include/NotifyManager.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef NOTIFYMANAGER_HPP_IPUA1IC9
-#define NOTIFYMANAGER_HPP_IPUA1IC9
-
-#include "Widgets/Notifier/NotifyManager.hpp"
-
-#endif /* end of include guard: NOTIFYMANAGER_HPP_IPUA1IC9 */
